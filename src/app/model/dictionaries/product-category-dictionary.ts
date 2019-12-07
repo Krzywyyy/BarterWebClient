247e28a337b4a717350abafd579a0d5c41c0982f
Piotr <krzywusiakk@gmail.com>
2019-12-07 18:33:29 +0100

    removed unnecessary dictionaries

--- src/app/model/dictionaries/product-category-dictionary.ts
+++
@@ -1,6 +0,0 @@
-export class ProductCategoryDictionary {
-    public static readonly BOOK: string = 'Książka';
-    public static readonly NOTES: string = 'Notatki';
-    public static readonly COACHING: string = 'Korepetycje';
-    public static readonly OTHER: string = 'Inne';
-}
